176090b5a3a86dce2a5d345f0c0c170a350e50ba
Pieter Reyneke <preyneke@gmail.com>
2018-05-07 15:23:15 +0200

    Chapter 2 Before Listing Customers

--- webstore/src/main/java/com/packt/webstore/config/DispatcherServletInitializer.java
+++ webstore/src/main/java/com/packt/webstore/config/DispatcherServletInitializer.java
@@ -8,7 +8,7 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		
-		return null;
+		return new Class[] {RootApplicationContextConfig.class};
 	}
 
 	@Override
--- webstore/src/main/java/com/packt/webstore/controller/ProductController.java
+++ webstore/src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,28 +1,35 @@
 package com.packt.webstore.controller;
 
-import java.math.BigDecimal;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.packt.webstore.domain.Product;
+
+import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.service.ProductService;
 
 @Controller
 public class ProductController {
 	
+	
+	
+	@Autowired
+	private ProductService productService;
+	
 	@RequestMapping("/products")
 	public String list(Model model) {
-		Product iphone = new Product("P1234", "iPhone x", new BigDecimal(500));
-		iphone.setDescription("Apple iPhone X smartphone with Super Retina HD display\r\n" + 
-				"5.8-inch (diagonal) all-screen OLED Multi-Touch display\r\n" + 
-				"HDR display and 12MP wide-angle and telephoto cameras\r\n");
-		iphone.setCategory("Smartphone");
-		iphone.setManufacturer("Apple");
-		iphone.setUnitsInStock(1000);
-		model.addAttribute("product", iphone);
+		
+		model.addAttribute("products", productService.getAllProducts());
 		
 		return "products";
+	}
+	@RequestMapping("/update/stock")
+	public String updateStock(Model model) {
+	productService.updateAllStock();
+	return "redirect:/products";
 	}
 
 }
